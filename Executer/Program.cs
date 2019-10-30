d4e733a347be84e628f7fb34a08429a8018bec60
Pietro Cattaneo <pieca314@gmail.com>
2019-10-30 12:41:17 +0000

    Added parameters check method

--- Executer/Program.cs
+++ Executer/Program.cs
@@ -1,23 +1,31 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Reflection;
 using MyAttribute;
 
 namespace Executer {
     class Program {
         static void Main(string[] args) {
-            var a = Assembly.LoadFrom("MyLibrary.dll");
+            var a = Assembly.LoadFrom("C:\\Users\\job3_\\Source\\Repos\\LabTaP1920_2\\MyLibrary\\bin\\Debug\\netstandard2.0\\MyLibrary.dll");
             foreach (var type in a.GetTypes()) {
                 if (type.IsClass)
                     Console.WriteLine(type.FullName);
                 var classInstance = Activator.CreateInstance(type);
                 MethodInfo[] methods = type.GetMethods();
                 foreach (var m in methods) {
-                    Console.WriteLine(m.ToString());
+                   // Console.WriteLine(m.ToString());
                     var methodAttributes = m.GetCustomAttributes<ExecuteMe>();
 
                     foreach (var cust in methodAttributes) {
-                        var methodPars = cust.GetMethodPar();
-                        m.Invoke(classInstance, methodPars);
+                        var methodAttrPars = cust.GetMethodPar();
+                        if (CheckMethodParams(m, methodAttrPars)) {
+                            m.Invoke(classInstance, methodAttrPars);
+                        }
+                        else {
+                            Console.WriteLine("Error in method parameters for method {0}," +
+                                              "skip to next method...\n", m.ToString());
+                        }
+
                     }
 
                 }
@@ -25,6 +33,18 @@ namespace Executer {
             }
 
             Console.ReadLine();
+        }
+
+        private static bool CheckMethodParams(MethodInfo m, object[] methodAttrPars) {
+            if (m.GetParameters().Length != methodAttrPars.Length)
+                return false;
+            int i = 0;
+            foreach (var parameterInfo in m.GetParameters()) {
+                if (parameterInfo.ParameterType != methodAttrPars[i].GetType())
+                    return false;
+                i++;
+            }
+            return true;
         }
 
     }
--- MyLibrary/FooClasses.cs
+++ MyLibrary/FooClasses.cs
@@ -24,4 +24,36 @@ namespace MyLibrary {
 
 
     }
+
+    public class Foo2 {
+        [ExecuteMe()]
+        public void M1_2() {
+            Console.WriteLine("M1");
+        }
+
+        [ExecuteMe(1, 2)]
+        public void M2_2(int a, int b) {
+            Console.WriteLine("M2 a={0} b={1}", a, b);
+        }
+
+        [ExecuteMe("hello", "reflection")]
+        public void M3_3(string s1, string s2) {
+            Console.WriteLine("M3 s1={0} s2={1}", s1, s2);
+        }
+
+    }
+
+    public class Foo3 {
+        [ExecuteMe("tre")]
+        public void M1_3(int a) {
+            Console.WriteLine("M1");
+        }
+
+        [ExecuteMe()]
+        public void M11024() {
+            Console.WriteLine("M1024");
+        }
+    }
+
+
 }
